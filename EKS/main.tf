2ed2c8f7f46b35986688254a05cfa7eb4648fca2
sameer59-saks <samo59git@gmail.com>
2024-10-19 08:49:14 -0400

    Adding eks dependency to avoid node buil failure

--- EKS/main.tf
+++ EKS/main.tf
@@ -55,8 +55,10 @@ module "eks" {
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
+  depends_on = [module.eks]
 }
